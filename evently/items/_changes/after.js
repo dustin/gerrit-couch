45b2ccc2fabd8efdd1d7596b313f7b04ca59954f
Dustin Sallings <dustin@spy.net>
2011-05-14 00:11:54 -0700

    Cleaned up the sidebar a lot.

--- evently/items/_changes/after.js
+++ evently/items/_changes/after.js
@@ -1,7 +1,4 @@
 function(me, args) {
-    var isChecked = $('#should_show_details:checked').val();
-    var newval = isChecked ? "block" : "none";
-    $('.extra-detail').css('display', newval);
     var app = $$(this).app;
     $("#mergeattempts").show();
     $("#activity").show();

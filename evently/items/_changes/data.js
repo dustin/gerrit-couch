39d2b203934a6c39cc9e24e07c0983bd9ea70665
Dustin Sallings <dustin@spy.net>
2011-05-15 00:34:59 -0700

    Graphically show approval info in the items list.

--- evently/items/_changes/data.js
+++ evently/items/_changes/data.js
@@ -13,10 +13,21 @@ function(data) {
         d.actor.img = gravatarURL(d.actor.email, 32);
         if (d.change) {
             var commentParts = [];
+            var approvals = [];
             for (var i = 0; d.approvals && i < d.approvals.length; ++i) {
                 var a = d.approvals[i];
                 if (a.value != '0') {
                     commentParts.push(a.description + ": " + a.value);
+                    var theClass = (a.value < 0 ? "negative" : "positive");
+                    if (a.type == 'VRIF') {
+                        approvals.push('<img alt="' + a.value + '"' +
+                                       ' src="images/VRIF' +
+                                       (a.value > 0 ? "%2b" : "") + a.value + '.png"' +
+                                      '/>');
+                    } else if (a.type == 'CRVW') {
+                        approvals.push('<span class="review ' + theClass + '">' +
+                                       (a.value > 0 ? "+" : "") + a.value + '</span>');
+                    }
                 }
             }
             if (d.comment != '') {
@@ -30,6 +41,10 @@ function(data) {
                 'link': d.change.url,
                 'linktext': d.change.subject
             };
+            d.approvals_h = approvals.sort(function(a, b) {
+                return (a.key > b.key) ? -1 : 1;
+            });
+            d.has_approvals = approvals.length > 1;
         }
 
         if (d.refUpdate) {

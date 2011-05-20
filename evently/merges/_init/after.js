084666fbd9271682ea4a81a6fc8f1d0ecb1e1d97
Dustin Sallings <dustin@spy.net>
2011-05-20 09:49:38 -0700

    Got rid of a fast-expanding reduce. Lame.
    This is a workaround for hard-coded limitations in couchdb that I hit
    when working with the android gerrit instance where they have lots of
    projects with long names.

--- evently/merges/_init/after.js
+++ evently/merges/_init/after.js
@@ -1,3 +1,15 @@
 function(me, args) {
-    showStreamGraph('mergechart', "#merges .legend", me.rows);
+    var data = [];
+    var prev = undefined;
+    var offset = -1;
+    me.rows.forEach(function(r) {
+        var k = [r.key[0], r.key[1]];
+        if (prev === undefined || k[0] != prev[0] || k[1] != prev[1]) {
+            prev = k;
+            ++offset;
+            data.push({key: k, value: {}});
+        }
+        data[offset].value[r.key[2]] = r.value;
+    });
+    showStreamGraph('mergechart', "#merges .legend", data);
 }
--- views/by-activity/map.js
+++ views/by-activity/map.js
@@ -3,7 +3,5 @@ function(doc) {
     if (doc.change) {
         branch = doc.change.branch;
     }
-    var ob = {};
-    ob[doc.project] = 1;
-    emit([doc.type, doc.ts.split('T')[0], doc.project, branch], ob);
+    emit([doc.type, doc.ts.split('T')[0], doc.project, branch], 1);
 }

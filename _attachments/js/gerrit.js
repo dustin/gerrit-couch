f1516bf03e61fd1ce206f69235b6bec231e16ff6
Dustin Sallings <dustin@spy.net>
2011-06-03 01:10:43 -0700

    Stop using evently for the charts.
    There's something weird that causes them to get rescheduled in ugly
    ways.  Just do it on demand.

--- _attachments/js/gerrit.js
+++ _attachments/js/gerrit.js
@@ -179,22 +179,180 @@ function updateTimestamps(app) {
     });
 }
 
-var pendingViewUpdate = undefined;
+function showActivityChart(app) {
+    var types = [
+        "patchset-created",
+        "comment-added",
+        "change-merged",
+        "ref-updated",
+        "change-abandoned",
+        "change-restored"
+    ];
+    // Look up the colors from the CSS (as applied on the page).
+    // We will use these to draw the graph
+    var colors = {};
+    for (var i = 0; i < types.length; ++i) {
+        colors[types[i]] = $('.' + types[i]).css('color');
+    }
+
+    app.view('activity', {reduce: true, group_level: 1,
+                          success: function(r) {
+                              showStreamGraph('activitychart',
+                                              '#activity .legend', r.rows,
+                                              function(d) { return colors[d]; });
+                          }});
+}
+
+function drawCollaborationChart(rows) {
+    var data = {}, totals = {}, projects = [];
+    var ymax = 0, collab_max = 0, total = 0;
+    rows.forEach(function (r) {
+        for (var k in r.value) {
+            var ob = data[k] || [];
+            ob.push([r.key[0][1], r.value[k]]);
+            data[k] = ob;
+            r.value._total += r.value[k];
+
+            totals[k] = (totals[k] || 0) + r.value[k];
+            ymax = Math.max(ymax, totals[k]);
+            collab_max = Math.max(collab_max, data[k].length);
+
+            if (projects.indexOf(k) == -1) {
+                projects.push(k);
+            }
+        }
+    });
+    for (var k in totals) {
+        total += totals[k];
+    }
+    projects.sort(function(a, b) {
+        var al = data[a].length, bl = data[b].length;
+        if (al == bl) {
+            return a > b ? -1 : 1;
+        } else {
+            return al - bl;
+        }
+    });
+
+    var angle = d3.scale.linear().domain([0, total]).range([0, Math.PI * 2]);
+    var named = 'collaborationchart';
+    var w = $('#' + named).width();
+    var h = w;
+    var radius = d3.scale.linear().domain([0, collab_max]).range([0, (w / 2)]);
+    var color = projectColorizer(projects);
+    var s = d3.scale.linear().domain([0, collab_max]).range([0, 20]);
+    var legend_prefix = '#collaboration .legend';
+
+    var prev = 0;
+    var angles = {};
+    var collab_levels = [];
+    // Initialize collab_levels -- map skips undefined vals.  :(
+    for (var i = 0; i < collab_max; ++i) { collab_levels.push(0); }
+    projects.forEach(function(p) {
+        var a = angle(totals[p]);
+        angles[p] = [prev, prev + a];
+        collab_levels[data[p].length] = Math.max(totals[p] + (collab_levels[data[p].length] || 0));
+        prev += a;
+    });
+
+    // Index the collaboration levels and remove 0 (since that's not helpful)
+    collab_levels = collab_levels.map(function(a, i) { return [i, a || 0, a || 0]; }).slice(1);
+    // Accumulate collaboration levels with the ones below
+    for (var i = 0; i < collab_levels.length; ++i) {
+        for (var j = i - 1; j >= 0; --j) {
+            collab_levels[j][1] += collab_levels[i][1];
+        }
+    }
+    prev = 0;
+    var collab_angles = collab_levels.map(function(l) {
+        var rv = [l[0], prev, prev + l[1]];
+        prev += l[2];
+        return rv;
+    });
+
+    $('#' + named).empty(); // Haven't quite figured out how to do this with D3
+    var vis = d3.select('#' + named)
+        .append("svg:svg")
+        .attr("width", w)
+        .attr("height", h)
+      .append("svg:g")
+        .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
+
+    var arc = d3.svg.arc()
+        .startAngle(function(d) { return angles[d][0]; })
+        .endAngle(function(d) { return angles[d][1]; })
+        .outerRadius(function(c, d) { return radius(data[c].length);});
+
+    var arcLines = d3.svg.arc()
+        .startAngle(function(d, p) { return angle(d[1]); })
+        .endAngle(function(d, p) { return angle(d[2]); })
+        .outerRadius(function(d, p) { return radius(d[0]);});
+
+    vis.selectAll(".lines")
+        .data(collab_angles)
+      .enter().append("svg:path")
+        .attr("class", "lines")
+        .attr("d", arcLines);
+
+    vis.selectAll(".slices")
+        .data(projects)
+      .enter().append("svg:path")
+        .attr("class", "slices")
+        .attr("d", arc)
+        .attr('fill', function(p) { return color(p); })
+        .on('mouseover', function(d, p) {
+            $(legend_prefix + " ." + projectToClass(d)).addClass("highlit");
+        })
+        .on('mouseout', function(d, p) {
+            $(legend_prefix + " ." + projectToClass(d)).removeClass("highlit");
+        });
+
+    // Update the legend
+    $(legend_prefix).empty();
+    projects.reverse();
+    projects.forEach(function(k) {
+        var people = data[k].length, changes = totals[k];
+        var style = "style='color: " + color(k) + "'";
+        $(legend_prefix).append("<span class='" + projectToClass(k) + "' " + style + ">" +
+                                k + "(" + people +"p, " + changes  + "c)</span> ");
+    });
+}
+
+function showCollaborationChart(app) {
+    app.view('contributor', {reduce: true, group_level: 1,
+                             success: function(r) {
+                                 drawCollaborationChart(r.rows);
+                             }});
+}
+
+function showMergeChart(app) {
+    app.view('by-activity', {reduce: true, group_level: 3,
+                             startkey: ["change-merged"], endkey: ["change-merged", {}],
+                             success: function(r) {
+                                 var data = [];
+                                 var prev = undefined;
+                                 var offset = -1;
+                                 r.rows.forEach(function(r) {
+                                     var k = [r.key[0], r.key[1]];
+                                     if (prev === undefined || k[0] != prev[0] || k[1] != prev[1]) {
+                                         prev = k;
+                                         ++offset;
+                                         data.push({key: k, value: {}});
+                                     }
+                                     data[offset].value[r.key[2]] = r.value;
+                                 });
+                                 showStreamGraph('mergechart', "#merges .legend", data);
+                             }});
+}
 
 function updateViews(app) {
-    if (!pendingViewUpdate) {
-        pendingViewUpdate = setTimeout(function() {
-            $("#activitychart").evently("activity", app);
-            $("#mergechart").evently("merges", app);
-            $("#collaborationchart").evently("collaboration", app);
-            $("#activity").show();
-            $("#merges").show();
-            $("#collaboration").show();
+    showActivityChart(app);
+    showCollaborationChart(app);
+    showMergeChart(app);
+    $("#activity").show();
+    $("#merges").show();
+    $("#collaboration").show();
 
-            $("#loading").hide();
-            $("#footer").show();
-
-            pendingViewUpdate = undefined;
-        }, 100);
-    }
+    $("#loading").hide();
+    $("#footer").show();
 }
